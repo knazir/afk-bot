6c2ccb7dcb9a6d74906101b423ff0a333ed3fe5b
Kashif Nazir <kashif@cs.stanford.edu>
2021-08-15 04:45:52 -0700

    Hooked up mongo, added guild and user collections, points command works

--- bot.js
+++ bot.js
@@ -1,43 +1,78 @@
+require("dotenv").config();
+
+/* ------------ Imports ------------ */
+
 const { Client, Collection, Intents } = require("discord.js");
 const fs = require("fs");
+const { MongoClient } = require("mongodb");
 const path = require("path");
 
-const { registerCommands } = require("./register");
+const { registerDevCommands } = require("./register");
 
-/* ------------ Setup ------------ */
+/* ------------ Parameters ------------ */
 
-require("dotenv").config();
+const DEFAULT_DB_NAME = "afkbot";
+const COLLECTION_NAMES = {
+  guilds: "guilds",
+  users: "users",
+};
+
+/* ------------ Global ------------ */
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const mongoUrl = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.rn139.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+let db = null;
+let guilds = null;
+let users = null;
 
-client.commands = new Collection();
-const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
-for (const file of commandFiles) {
-  const command = require(path.join(__dirname, "commands", file));
-  client.commands.set(command.data.name, command);
-}
-
-/* ------------ Commands ------------ */
+/* ------------ Event Handlers ------------ */
 
 client.on("interactionCreate", async interaction => {
   if (!interaction.isCommand()) return;
-  const { commandName } = interaction;
+  const { commandName, guild: iGuild, user: iUser } = interaction;
   if (!client.commands.has(commandName)) return;
 
   try {
-    await client.commands.get(commandName).execute(interaction);
+    const guild = (await guilds.findOneAndUpdate(
+      { id: iGuild.id },
+      { $set: { lastName: iGuild.name }, $setOnInsert: { id: iGuild.id, bets: {} } },
+      { upsert: true },
+    )).value;
+    const user = (await users.findOneAndUpdate(
+      { id: iUser.id },
+      { $set: { lastUsername: iUser.username }, $setOnInsert: { id: iUser.id, points: 0 } },
+      { upsert: true },
+    )).value;
+    await client.commands.get(commandName).execute(interaction, { db, guilds, users, guild, user });
   } catch (error) {
     console.error(error);
     await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true })
   }
 });
 
-/* ------------ Startup ------------ */
-
-client.on("ready", () => {
-  console.log("Successfully logged in");
+client.on("ready", async () => {
+  console.log("Successfully logged in.");
 });
 
-(async () => {
-  await registerCommands();
+/* ------------ Setup ------------ */
+
+MongoClient.connect(mongoUrl, async (connectionError, dbClient) => {
+  if (connectionError) throw connectionError;
+  console.log("Successfully connected to database.");
+
+  // Setup database
+  db = dbClient.db(process.env.DB_NAME || DEFAULT_DB_NAME);
+  guilds = db.collection(COLLECTION_NAMES.guilds);
+  users = db.collection(COLLECTION_NAMES.users);
+
+  // Setup commands
+  client.commands = new Collection();
+  fs.readdirSync("./commands").filter(file => file.endsWith(".js")).forEach(file => {
+    const command = require(path.join(__dirname, "commands", file));
+    client.commands.set(command.data.name, command);
+  });
+
+  // Only start the bot once the database is ready
+  await registerDevCommands();
   return client.login(process.env.DISCORD_TOKEN);
-})();
+});
--- register.js
+++ register.js
@@ -16,13 +16,13 @@ for (const file of commandFiles) {
 }
 
 module.exports = {
-  async registerCommands() {
+  async registerDevCommands() {
     try {
-      console.log("Started refreshing application (/) commands.");
       await rest.put(
         Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.DEV_GUILD_ID),
         { body: commands },
       );
+      console.log("Successfully registered / commands.");
     } catch (error) {
       console.error(error);
     }
